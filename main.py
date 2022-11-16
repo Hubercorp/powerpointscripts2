bd609eb32ff0c58956606c40a927d6efae0e0f69
Hubercorp <hubertyj@gmail.com>
2022-11-16 11:16:57 +0100

    fully working - link missing

--- main.py
+++ main.py
@@ -9,19 +9,16 @@ print(file_list_cleaned)
 df = pd.DataFrame()
 
 
-for i, ppt in enumerate(file_list_cleaned):
+for ppt in file_list_cleaned:
     ppt_app = win32com.client.GetObject(path.decode('utf8') +"\\" + ppt)
-    df.loc[i,'00_File'] = ppt
-    print("----Ppt name----",ppt)
     for j, ppt_slide in enumerate(ppt_app.Slides):
-        df.loc[i,'00_Slide'] = j
-        print("----Slide number----", ppt_slide, '--#--', j)
         for k, comment in enumerate(ppt_slide.Comments):
-            print("----Comment----", ppt_slide, '--#--', k)
-            df.loc[i, str(k)+"_Comment" ] = comment.Text
-            for l, reply in enumerate(comment.Replies):
-                print("----Reply---", ppt_slide, '--#--', l)
-                df.loc[i,str(l)+"_Replies"] = reply.Text
+            if comment is not None:
+                df.loc[j,'00_File'] = ppt
+                df.loc[j,'00_Slide'] = j
+                df.loc[j, str(k)+"_Comment" ] = comment.Text
+                for l, reply in enumerate(comment.Replies):
+                    df.loc[j,str(l)+"_Replies"] = reply.Text
 
 
 df = df.reindex(sorted(df.columns), axis=1)
